d4699989108e97a1c9e1df78cc0c710f1d9d8609
RyanFlachman <ryan@flachman.net>
2019-07-26 14:39:50 -0600

    Add ItemCard styling

--- src/Components/ItemCard/ItemCard.js
+++ src/Components/ItemCard/ItemCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './ItemCard.scss'
 
 export const ItemCard = ({item}) => {
   return (
-    <article>
-      <h1>{item.name}</h1>
+    <article className="item-card">
+      <img src={item.sprites.default} alt={item.name} className="item-card__image"/>
+      <h1 className="item-card__name">{item.name}</h1>
     </article>
   )
 }

1cc708f3e862d5364eb74c8ca4647846e085ab2e
RyanFlachman <ryan@flachman.net>
2019-07-30 07:53:31 -0600

    Add pokemon thunks

--- src/Thunks/Thunks.test.js
+++ src/Thunks/Thunks.test.js
@@ -1,6 +1,8 @@
 import { getGamesThunk } from './gameThunks'
 import { getMovesThunk } from './movesThunk'
 import { getRegionsThunk } from './regionsThunk'
+import { getPokeThunk, getMorePokemonThunk, addPokemonThunk } from './pokemonThunks'
+import { getItemsThunk, getItemsbyCategory } from './itemThunks'
 
 describe('All Thunks', () => {
   describe('getGamesthunk', () => {
@@ -63,6 +65,92 @@ describe('All Thunks', () => {
           json: () => Promise.resolve(mockResponse)
         })
       })
+    })
+
+    it('should be called with the correct params', async () => {
+      const url = 'https://pokeapi.co/api/v2/region'
+      await getRegionsThunk()(mockDispatch)
+      expect(window.fetch).toHaveBeenCalledWith(url)
+    })
+
+    it('should dispatch the getRegions action', async () => {
+      const mockAction = {type: 'GET_REGIONS', data: [mockResponse]}
+      await getRegionsThunk()(mockDispatch)
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction)
+    })
+  })
+
+  describe('Pokemon Thunks', () => {
+    let mockResponse, mockDispatch;
+    beforeEach(() => {
+      mockResponse = {results: [{name: 'charmander'}]}
+      mockDispatch = jest.fn()
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockResponse)
+        })
+      })
+    })
+
+    it('getPokeThunk should be called with the correct params', async () => {
+      const url = 'https://pokeapi.co/api/v2/pokemon'
+      await getPokeThunk()(mockDispatch)
+      expect(window.fetch).toHaveBeenCalledWith(url)
+    })
+
+    it('getPokeThunk should dispatch the getPokemon action', async () => {
+      const mockAction = {type: 'GET_POKEMON', data: [mockResponse]}
+      await getPokeThunk()(mockDispatch)
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction)
+    })
+
+    it('getMorePokemonThunk should be called with the correct params', async () => {
+      const url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
+      await getMorePokemonThunk(0)(mockDispatch)
+      expect(window.fetch).toHaveBeenCalledWith(url)
+    })
+
+    it('getMorePokemonThunk should dispatch the getPokemon action', async () => {
+      const mockAction = {type: 'GET_POKEMON', data: [mockResponse]}
+      await getMorePokemonThunk(0)(mockDispatch)
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction)
+    })
+
+    it('addPokemonThunk should be called with the correct params', async () => {
+      const url = 'https://pokeapi.co/api/v2/pokemon/charmander/'
+      await addPokemonThunk('charmander', 'add pokemon')(mockDispatch)
+      expect(window.fetch).toHaveBeenCalledWith(url)
+    })
+
+    it('addPokemonThunk should dispatch addPokemon action when action param is add pokemon', async () => {
+      const mockAction = {type: 'ADD_POKEMON', data: mockResponse}
+      await addPokemonThunk('charmander', 'add pokemon')(mockDispatch)
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction)
+    })
+
+    it('addPokemonThunk should dispatch addPokedex action when action param isnt add pokemon', async () => {
+      const mockAction = {type: 'ADD_POKEDEX', data: mockResponse}
+      await addPokemonThunk('charmander', 'add pokedex')(mockDispatch)
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction)
+    })
+  })
+
+  describe('Item Thunks', () => {
+    let mockResponse, mockDispatch;
+    beforeEach(() => {
+      mockResponse = {results: [{name: 'poke ball'}]}
+      mockDispatch = jest.fn()
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockResponse)
+        })
+      })
+    })
+
+    it('getItemsThunk should be called with the correct params', async () => {
+      const url = 'https://pokeapi.co/api/v2/item'
+      await getItemsThunk()(mockDispatch)
+      expect(window.fetch).toHaveBeenCalledWith(url)
     })
   })
 })

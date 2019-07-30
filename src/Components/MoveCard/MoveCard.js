a89036b95347a3266fc0e020b2338fc484dbfa38
RyanFlachman <ryan@flachman.net>
2019-07-30 08:15:31 -0600

    Add move card styling

--- src/Components/MoveCard/MoveCard.js
+++ src/Components/MoveCard/MoveCard.js
@@ -5,7 +5,7 @@ export const MoveCard = ({move}) => {
   return (
     <article className="move-card">
       <h2 className="move-card__name">{move.name}</h2>
-      <p className="move-card__type">{move.type.name}</p>
+      <p className="move-card__type">type: {move.type.name}</p>
     </article>
   )
 }

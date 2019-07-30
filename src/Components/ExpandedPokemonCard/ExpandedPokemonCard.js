95a2ce1f075d8bab60e0f74e3447f2dbe00b7188
RyanFlachman <ryan@flachman.net>
2019-07-29 22:25:09 -0600

    Add expanded pokemon container and styling

--- src/Components/ExpandedPokemonCard/ExpandedPokemonCard.js
+++ src/Components/ExpandedPokemonCard/ExpandedPokemonCard.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { getPokemonByName } from '../../apiCalls'
+import './ExpandedPokemonCard.scss'
+import { NavLink } from 'react-router-dom'
 
 export class ExpandedPokemonCard extends React.Component {
   constructor() {
     super();
     this.state = {
-      pokemon: {}
+      pokemon: {
+        name: '',
+        moves: [],
+        stats: [],
+        sprites: []
+      }
     } 
   }
 
@@ -13,11 +20,50 @@ export class ExpandedPokemonCard extends React.Component {
     getPokemonByName(this.props.pokemon)
       .then(data => this.setState({pokemon: data}))
   }
+
+  displayMoveList() {
+    return this.state.pokemon.moves.map( move => {
+        console.log(move.move.name)
+        return <NavLink to={`/moves/${move.move.name}`} className="expanded-move-button">{move.move.name}</NavLink>
+      })
+  }
+
+  displayStats() {
+    return this.state.pokemon.stats.map( stat => {
+      return <li className="expanded-base-stats">{stat.stat.name}: {stat.base_stat}</li>
+    })
+  }
+
+  displaySprites() {
+    return Object.entries(this.state.pokemon.sprites).map( sprite => {
+      if (sprite[1] !== null) {
+      return <img src={sprite[1]} alt={this.state.pokemon.name} className="expanded-sprite"/>
+      }
+    })
+  }
   render() {
-    console.log(this.state.pokemon)
     return (
       <article>
-        <h2>{this.state.pokemon.name}</h2>
+        <h1 className="expanded-pokemon-name">{this.state.pokemon.name.toUpperCase()}</h1>
+        <section className="expanded-poke-container">
+          <div className="expanded-move-list-container">
+            <h2 className="move-list-header">Move List</h2>
+            <ul className="expanded-move-list">
+              {this.displayMoveList()}
+            </ul>
+          </div>
+          <div className="expanded-stat-container">
+            <h2 className="expanded-stat-header">Base Stats</h2>
+            <ul className="display-stats-list">
+              {this.displayStats()}
+            </ul>
+          </div>
+          <div className="expanded-sprite-container">
+            <ul>
+              {this.displaySprites()}
+            </ul>
+          </div>
+        </section>
       </article>
     )
   }
